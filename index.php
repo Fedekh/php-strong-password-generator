d877080d3441ecb36a3125aadb598d8261dd184e
Fedekh <federicocet@gmail.com>
2023-05-05 16:15:54 +0200

    insert php tag

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+$pass = '';
+if (isset($_GET['user-password'])) {
+    if (strlen($_GET['user-password']) >= 8) {
+        $pass = $_GET['user-password'];
+        echo $pass;
+    }
+    else {
+        echo 'Password troppo corta';
+    }
+}
+
+var_dump($pass);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,8 +22,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
     <!-- CUSTOM CSS -->
     <link rel="stylesheet" href="./css/style.css">
@@ -16,7 +30,7 @@
     <title>STRONG PASSWORD GENERATOR</title>
 </head>
 
-<body>
+<body class="text-white">
 
     <div class="wrapper d-flex my-5 justify-content-center text-center align-items-center">
         <div class="ms_container d-flex-column">
@@ -30,17 +44,17 @@
                 <form method="GET" action="index.php">
                     <div class="input-field d-flex flex-column align-items-center">
                         <label class="long" for="long">Inserisci la lunghezza password desiderata:</label>
-                        <input id="long" name="pass" class="rounded border-0 my-3 w-25" type="text">
+                        <input id="long" name="user-password" class="rounded border-0 my-3 w-25" type="text">
                     </div>
                     <div class="buttons my-5 mx-auto text-center">
                         <button type="submit" class="btn btn-primary align-self">Invia</button>
                         <button type="button" class="btn mx-3 btn-danger align-self"><a href="index.php">RESET</a></button>
                     </div>
                 </form>
+
             </div>
 
         </div>
-    </div>
     </div>
 </body>
 
